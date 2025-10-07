0fede884e436e51e308c8ae77f8265aa82bab15b
Sparsh Singh <sparshsingh7586@gmail.com>
2025-10-07 15:01:32 +0530

    Fixed an uninitilized page

---
+++ web/app/data/page.tsx
@@ -0,0 +1,4 @@
+const Data = () => {
+	return <h1> This is the data page </h1>
+}
+export default Data;
---
+++ web/app/staff/page.tsx
@@ -0,0 +1,5 @@
+const Staff = () => {
+	return <h1> This is the staff page</h1>
+}
+
+export default Staff;
